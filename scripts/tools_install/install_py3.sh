11dad86badb448b47d9a05c03034ec8c7176cef2
murshidazher <murshidazher@yahoo.com>
2021-07-12 04:23:45 +0530

    :sparkles: feat: update notebook

--- scripts/tools_install/install_py3.sh
+++ scripts/tools_install/install_py3.sh
@@ -9,7 +9,7 @@ yum -y remove python36u python36u-devel python36u-pip python36u-setuptools pytho
 yum -y install python36 python36-devel python36-pip python36-setuptools python36-tools python36-libs python36-tkinter gcc-c++
 yum -y install cmake3 cmake3-data
 python3 --version
-pip3 install wheel pandas numpy scipy scikit-learn matplotlib virtualenv
+pip3 install wheel pandas numpy scipy scikit-learn matplotlib virtualenv seaborn
 
 ####========================================================
 ### Install Jupyter
