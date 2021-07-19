9d9ee6408a1a3f6cc4a167c80f64af93ef9c96f1
murshidazher <murshidazher@yahoo.com>
2021-07-19 23:50:35 +0530

    :sparkles: feat: add beeline script

--- scripts/tools_install/setup_scripts.sh
+++ scripts/tools_install/setup_scripts.sh
@@ -1,7 +1,6 @@
 # bin/bash
 
-mkdir -p hw-workspace/input/warehouse/stock_dataset/file-formats/text &&
-  mkdir -p hw-workspace/input/warehouse/stock_dataset/file-formats/orc &&
+mkdir -p hw-workspace/input/warehouse/stock_dataset/file-formats &&
   mkdir -p hw-workspace/input/stocks-dataset
 
 cd hw-workspace &&
@@ -13,4 +12,5 @@ hadoop fs -mkdir -p /user/maria_dev/hw-workspace/input/warehouse/stock_dataset/f
 
 hadoop fs -copyFromLocal ~/hw-workspace/hdp-stocks-scripts/warehouse/file-format/orc/* hw-workspace/input/stocks-dataset/file-formats/orc/
 
+# beeline -u jdbc:hive2://sandbox-hdp.hortonworks.com:10000/ -n maria_dev -f ~/hw-workspace/hdp-stocks-scripts/scripts/hive/LoadingStocksORC.sql
 hive -f ~/hw-workspace/hdp-stocks-scripts/scripts/hive/LoadingStocksORC.sql
