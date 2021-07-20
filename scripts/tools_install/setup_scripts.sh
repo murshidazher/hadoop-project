3372fef3098183335ebcf39eeefe6ad1b948afea
murshidazher <murshidazher@yahoo.com>
2021-07-20 22:29:59 +0530

    :sparkles: feat: update the script

--- scripts/tools_install/setup_scripts.sh
+++ scripts/tools_install/setup_scripts.sh
@@ -1,14 +1,13 @@
 # bin/bash
 
-mkdir -p hw-workspace/input/warehouse/stock_dataset/file-formats &&
-  mkdir -p hw-workspace/input/stocks-dataset
-
 # cd hw-workspace &&
 #   git clone https://github.com/murshidazher/hdp-stocks-scripts.git
 
-hadoop fs -mkdir -p /user/maria_dev/hw-workspace/input/warehouse/stock_dataset/file-formats
+cd ~
 
-hadoop fs -mkdir -p /user/maria_dev/hw-workspace/input/warehouse/stock_dataset/file-formats/orc
+hadoop fs -mkdir -p hw-workspace/input/warehouse/stock_dataset/file-formats
+
+hadoop fs -mkdir -p hw-workspace/input/warehouse/stock_dataset/file-formats/orc
 
 hadoop fs -copyFromLocal ~/hw-workspace/hdp-stocks-scripts/warehouse/file-format/orc/* hw-workspace/input/stocks-dataset/file-formats/orc/
 
